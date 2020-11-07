463f4f2af9f46542aa18717e35efd35f70de62f4
Jamie Anderson <andejt1@student.op.ac.nz>
2020-11-08 02:14:41 +1300

    js update, stream can be select via dropdown, attendance marks can be inserted, submit button due to implement.

--- _includes/javascript.js
+++ _includes/javascript.js
@@ -5,38 +5,46 @@ $.ajax({
     dataType: 'json',
     success: function(data) {
     console.log(data);
+    //Variables for previous attendance array.
     var pc1 = 0, pc2 = 1, pc3 = 2, pc4 = 3;
+    //Variables for code to be inserted into table.
     var tdstream = "<td class='streamopt'></td>"
-    //var stream = "<td><select id='streamopt' onchange='streamcheck()'><option value=''>N/A</option><option value='a'>A</option><option value='b'>B</option></select></td>"
-    var attendance = "<td><select><option value=''>N/A</option><option value='p'>P</option><option value='e'>E</option><option value='a'>A</option></select></td>";
+    var attendance = "<td><select><option value=''>N/A</option><option value='p'>Present</option><option value='l'>Late</option><option value='e'>Explained Abs</option><option value='a'>Absent</option></select></td>";
+    //Row count function.
+    var rowCount = $('#roletable >tbody >tr').length;
+    console.log(rowCount);
     for (var i = 0; i < data.length; i++) {
-        if((data[i].class == 'IN790') || (data[i].isActive == 'true')){           
+        if ((data[i].class == 'IN790') || (data[i].isActive == 'true')) {           
                 $("#sturole").append("<tr><td>" + data[i].name.first + " " + data[i].name.last + tdstream + attendance + 
-                "<td>" + data[i].attendance[pc1] +"</td><td>"+ data[i].attendance[pc2] +"</td><td>"+ data[i].attendance[pc3] +
-                "</td><td>"+ data[i].attendance[pc4] +  "</td></tr>");
-                console.log(i)
-        };
-        
+                "<td id='pa1'>" + data[i].attendance[pc1] +"</td><td id='pa2'>"+ data[i].attendance[pc2] +"</td><td id='pa3'>"+ data[i].attendance[pc3] +
+                "</td><td id='pa4'>"+ data[i].attendance[pc4] +  "</td></tr>");
+               // if (pa1.innerHTML == 'a'){
+                //  console.log("check!")
+                //}
+        }
     };
-     }
+}
 });
 
-
-
+//streamcheck() on stream option change, all students are autofilled to that stream with assigned value.
 function streamcheck(){
     var rowCount = $('#roletable >tbody >tr').length;
     console.log("Row:", rowCount);
+    //If stream is selected as 'a' by the user, statement will perform. 
     if (document.getElementById("streamsel").value == 'a'){
+        //Loops all rows in table for stream option.
         for (var c = 0; c < rowCount; c++) {
+            //Replaces displayed stream value with 'a'.
             $('.streamopt').replaceWith("<td class='streamopt'>a</td>")
         }
-        
     }
+    //same as above, except value is 'b'.
     else if (document.getElementById("streamsel").value == 'b'){
         for (var c = 0; c < rowCount; c++) {
             $('.streamopt').replaceWith("<td class='streamopt'>b</td>")
         }
     }
+    //same as above, except value is 'null'.
     else if (document.getElementById("streamsel").value == ''){
         for (var c = 0; c < rowCount; c++) {
             $('.streamopt').replaceWith("<td class='streamopt'></td>")
